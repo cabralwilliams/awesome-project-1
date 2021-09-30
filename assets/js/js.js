0dce9cc6753b672b8a3e7c7edb20c0a19a2e6148
sonali_sohoni <ssohoni@camsbycbs.com>
2021-09-30 11:09:40 -0500

    comments - checking push pull merge

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,69 +1,62 @@
-var dia=moment().format('l');
+var dia = moment().format("l");
 
 //load page first time
-var loadpage=function(){
-}
+var loadpage = function () {};
 
-//submit botton 
-var formSubmitHandler = function(event) {
-      // prevent page from refreshing
-      event.preventDefault();
-      // get value from input element
-         
+//submit botton
+var formSubmitHandler = function (event) {
+	// prevent page from refreshing
+	event.preventDefault();
+	// get value from input element
 };
-
+//When user clicks on search date funciton , open date picker modal box -Sonali
 //Api urls, keys, and general search information
 //History api usage: historyUrl + "9/29" -> would search for notable people events of September 29
 var historyUrl = "http://history.muffinlabs.com/date/";
 
-//load city to get latitud and longitud to be able to display 5 days 
-var getcity = function(city) {
-  // format the weather api url by city
-  var apiUrl=  "https://api.openweathermap.org/data/2.5/weather?"
-  // make a get request to url
-  fetch(apiUrl)
-    .then(function(response) {
-      // request was successful
-      if (response.ok) {
-          response.json().then(function(data) {
-          
-          
-        });
-      } else { //api response returned errors
-        alert("Error: " + response.statusText);
-      }
-    })
-    .catch(function(error) {
-      alert("Unable to connect to Weather Web site");
-    });
+//load city to get latitud and longitud to be able to display 5 days
+var getcity = function (city) {
+	// format the weather api url by city
+	var apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
+	// make a get request to url
+	fetch(apiUrl)
+		.then(function (response) {
+			// request was successful
+			if (response.ok) {
+				response.json().then(function (data) {});
+			} else {
+				//api response returned errors
+				alert("Error: " + response.statusText);
+			}
+		})
+		.catch(function (error) {
+			alert("Unable to connect to Weather Web site");
+		});
 };
 
 //save array to local storage
 
-
 //using lat and lon, get all info for current day and next 5 days
-var getmoredetails = function() {
-  // format the github api url
-  var apiUrl= "https://api.openweathermap.org/data/2.5/onecall";
-  // make a get request to url
-  fetch(apiUrl).then(function(response) {
-    // request was successful
-    if (response.ok) {
-      response.json().then(function(data) {
-        console.log(data);
-        });
-    } else {
-      alert("Error: " + response.statusText);
-    }
-  });
+var getmoredetails = function () {
+	// format the github api url
+	var apiUrl = "https://api.openweathermap.org/data/2.5/onecall";
+	// make a get request to url
+	fetch(apiUrl).then(function (response) {
+		// request was successful
+		if (response.ok) {
+			response.json().then(function (data) {
+				console.log(data);
+			});
+		} else {
+			alert("Error: " + response.statusText);
+		}
+	});
 };
 
-//create buttons and save new data on array 
+//create buttons and save new data on array
 
 //load data from previously saved data
 
 // add event listeners to form and button container
 
-
 // load for the first time
-
